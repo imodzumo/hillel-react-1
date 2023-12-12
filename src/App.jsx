2ca4b129645447391a6873259e8b22069c832965
igor <igor.angelchev@tateeda.com>
2023-12-12 21:33:07 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Input from "./components/Input/Input.jsx";
 import Popup from "./components/Popup/Popup.jsx";
 import {useState} from "react";
+import Button from "./components/Button/Button.jsx";
+import Card from "./components/Card/Card.jsx";
 
 const App = () => {
     const [name, setName] = useState('');
@@ -16,10 +18,30 @@ const App = () => {
         setIsPopupOpen(false);
     };
 
+    const userInfo = {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        age: 30,
+        skills: [
+            {id: 1, title: 'React', level: 1},
+            {id: 2, title: 'Ts', level: 2},
+            {id: 3, title: 'Redux', level: 3}
+        ]
+    }
+
+    const handleSubmitForm = (event) => {
+        event.preventDefault()
+        console.log(123);
+    }
+
     return (
         <div className="main-container">
-            {!isPopupOpen && <Input onNameSubmit={handleNameSubmit} />}
-            {isPopupOpen && <Popup name={name} onClose={closePopup} />}
+            {/*{!isPopupOpen && <Input onNameSubmit={handleNameSubmit} />}*/}
+            {/*{isPopupOpen && <Popup name={name} onClose={closePopup} />}*/}
+            {/*<Button text="Cancel" placeholder="Button" isLoading={true} count={20}/>*/}
+            {/*<Button text="Submit"/>*/}
+            {/*<Button/>*/}
+            <Card info={userInfo} onSubmitForm={handleSubmitForm}/>
         </div>
     );
 }
