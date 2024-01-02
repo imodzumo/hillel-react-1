9db12dc89c743587bdf4a15d275ef07f889257b1
igor <igor.angelchev@tateeda.com>
2024-01-02 02:06:02 +0200

    added loader

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,37 @@
 import UsersList from "../components/UsersList/UsersList.jsx";
 import {Link} from "react-router-dom";
+import { useState, useEffect } from 'react';
+
 const Users = ()=> {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = ()=> {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 1000);
+
+            clearTimeout(timer);
+        }
+
+        timer();
+    }, []);
 
     return (
-        <div className="users-wrapper">
-            <UsersList/>
-            <Link to="/" className="button-link">Go to Homepage</Link>
+        <div>
+            {isLoading ?
+                <div className="loading-wrapper">
+                    <p>Loading...</p>
+                    <div className="loader-container">
+                        <div className="loader-bar"></div>
+                    </div>
+                </div>
+             : (
+                 <div className="users-wrapper">
+                     <UsersList />
+                     <Link to="/" className="button-link">Go to Homepage</Link>
+                 </div>
+            )}
         </div>
     );
 }
